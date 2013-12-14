319228192b9c4305fc09d1761f77a1d5ecb97968
Mr. Spothawk <mr.spothawk@gmail.com>
2013-12-13 19:39:38 -0800

    added css to links. values dispalying

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,12 +10,23 @@ angular.module('shortlyApp')
       $scope.links = data.data;
       // console.info(data.data)
     })
+    // $scope.clickCount = function($scope, $http){
+    //   $http({
+    //   method: 'POST',
+    //   url: '/links',
+    //   data: {url: this.text}
+
+    // })
+    //  .then(function(data){
+    //   console.info('data ', arguments)
+    //   // console.log(data, ' that data, wow!')
+    //  }) 
+    // }
   })
   .controller('ShortenController', function($scope, $http){
     $scope.name='Shorten';
     $scope.text='balls';
     $scope.submit = function() {
-      console.log(this.text);
       $http({
       method: 'POST',
       url: '/links',
