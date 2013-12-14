ba02ca851a0b1cef7bb84b0ed941d69b2b9f2fe4
Mr. Spothawk <mr.spothawk@gmail.com>
2013-12-13 17:32:18 -0800

    able to post new links now.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,9 +8,25 @@ angular.module('shortlyApp')
     })
     .then(function(data) {
       $scope.links = data.data;
+      // console.info(data.data)
     })
   })
   .controller('ShortenController', function($scope, $http){
     $scope.name='Shorten';
+    $scope.text='balls';
+    $scope.submit = function() {
+      console.log(this.text);
+      $http({
+      method: 'POST',
+      url: '/links',
+      data: {url: this.text}
+
+    })
+     .then(function(data){
+      console.info('data ', arguments)
+      // console.log(data, ' that data, wow!')
+     }) 
+    };
+
   })
   
